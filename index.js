f71e44f9f333123906281f5c257bffe34af15111
Adam Young <adamyoung1414@gmail.com>
2023-12-22 16:28:11 -0500

    added global variables for storing the calculator information added a function operate that calls, add, subtract, multiply, or divide

--- index.js
+++ index.js
@@ -14,3 +14,13 @@ function multiply (a, b) {
 function divide (a, b) {
     return a / b;
 }
+
+// function to run the calculator
+function operate(operator, firstNumber, secondNumber) {
+    return window[operator](firstNumber, secondNumber);
+}
+
+// global variables
+let firstNumber = 0;
+let operator = '';
+let secondNumber = 0;
